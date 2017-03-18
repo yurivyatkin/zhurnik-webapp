e6b048dcc4e2cd8bc3b0697ec63e0b03419a4337
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2017-03-18 21:15:59 +1300

    Enable Standard JS linting

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'sourceType': 'module',
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+  },
+  'rules': {
+    'comma-dangle': ['error', {
+      'arrays': 'always-multiline',
+      'objects': 'always-multiline',
+      'imports': 'always-multiline',
+      'exports': 'always-multiline',
+      'functions': 'ignore',
+    }],
+  },
+  'extends': [
+    'standard',
+    'standard-react',
+  ],
+  'plugins': [
+    'promise',
+    'react',
+  ],
+  'env': {
+    'jest': true,
+  },
+}
