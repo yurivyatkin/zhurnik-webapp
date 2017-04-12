8c9bfda5fbdf2ddbddbabb6b8a5294b100c80ba4
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2017-04-12 23:49:22 +1200

    Make App component  initialize currentBuffer with the currentDate's note content

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
+import notesService from '../services/notes'
+import { changeBuffer } from '../store/editor/actions'
+import { getCurrentDate } from '../store/calendar/reducer'
+import store from '../store'
 import Dashboard from './Dashboard'
 import TextPane from './TextPane'
 import './App.css'
 
 class App extends Component {
+  componentDidMount () {
+    const initialDate = getCurrentDate(store.getState())
+    const initialBuffer = notesService.getNoteByDateFromLocalStorage(initialDate)
+    store.dispatch(changeBuffer(initialBuffer))
+  }
+
   render () {
     return (
       <div>
@@ -14,4 +24,4 @@ class App extends Component {
   }
 }
 
-export default App
+export default (App)
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,5 +1,9 @@
+import calendar from './calendar/reducer'
+import editor from './editor/reducer'
 import notes from './notes/reducer'
 
 export {
+  calendar,
+  editor,
   notes,
 }
