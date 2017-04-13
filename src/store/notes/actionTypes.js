5fcdbda7753ecfa3786c8b9c051c08c741a952f1
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2017-04-13 19:42:30 +1200

    Remove Notes from the store
    Thus completed refactoring the store to have two features: calendar (dates) and editor (text)

--- src/store/notes/actionTypes.js
+++
@@ -1,7 +0,0 @@
-export const CHANGE_NOTE = 'CHANGE_NOTE' // Change the content when editing
-export const CREATE_NOTE = 'CREATE_NOTE' // Create a new note on the server
-export const FETCH_NOTE = 'FETCH_NOTE' // Retrieve from the server
-export const SET_NOTE = 'SET_NOTE' // Load the note's content
-export const SWITCH_NOTE = 'SWITCH_NOTE' // Load another note when the date is changed
-export const UPDATE_NOTE = 'UPDATE_NOTE' // Save the content to the server
-export const TOGGLE_MODE = 'TOGGLE_MODE' // Preview or edit
