327bafc71b058c57da5371fc9dfc0ebb1a93ab39
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2017-04-14 15:16:01 +1200

    Save buffer if edited on closing the app to the current date's note
    The action of changing the buffer marks it as editor once (until saved)

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react'
 import notesService from '../services/notes'
-import { changeBuffer } from '../store/editor/actions'
+import { changeBuffer, clearEdited } from '../store/editor/actions'
 import { getCurrentDate } from '../store/calendar/reducer'
+import { getCurrentBuffer, isEdited } from '../store/editor/reducer'
 import store from '../store'
 import Dashboard from '../containers/Dashboard'
 import Editor from '../containers/Editor'
 import './App.css'
 
 class App extends Component {
+  onUnload () {
+    if (isEdited(store.getState())) {
+      const state = store.getState()
+      const currentDate = getCurrentDate(state)
+      const currentBuffer = getCurrentBuffer(state)
+      notesService
+        .setNoteByDateToLocalStorage(currentDate, currentBuffer)
+      return null
+    } else {
+      return null
+    }
+  }
+
   componentDidMount () {
+    // Load today's note when the app has started
     const initialDate = getCurrentDate(store.getState())
     const initialBuffer = notesService.getNoteByDateFromLocalStorage(initialDate)
     store.dispatch(changeBuffer(initialBuffer))
+    store.dispatch(clearEdited()) // because the note is just loaded
+    // Add handling app's closing events
+    window.onbeforeunload = this.onUnload
   }
 
   render () {
--- src/store/editor/actions.js
+++ src/store/editor/actions.js
@@ -1,6 +1,13 @@
 import * as types from './actionTypes'
+import store from '../../store'
+import { isEdited } from '../editor/reducer'
 
 export function changeBuffer (buffer) {
+  // Mark as edited once
+  if (!isEdited(store.getState())) {
+    store.dispatch(markEdited())
+  }
+
   return {
     type: types.CHANGE_BUFFER,
     buffer,
