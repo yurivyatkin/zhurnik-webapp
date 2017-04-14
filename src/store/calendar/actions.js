b7196b5b93ccb19a23aa3e21c3c8cd3708941e8e
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2017-04-14 13:39:03 +1200

    Save buffer when changing date, and load the new date's buffer
    Straightforwardly do this in the changeDate action creator

--- src/store/calendar/actions.js
+++ src/store/calendar/actions.js
@@ -1,6 +1,20 @@
 import * as types from './actionTypes'
+import store from '../../store'
+import { getCurrentDate } from './reducer'
+import { getCurrentBuffer } from '../editor/reducer'
+import notesService from '../../services/notes'
+import { changeBuffer } from '../editor/actions'
 
 export function changeDate (date) {
+  // Save the current note
+  const state = store.getState()
+  const currentDate = getCurrentDate(state)
+  const currentBuffer = getCurrentBuffer(state)
+  notesService.setNoteByDateToLocalStorage(currentDate, currentBuffer)
+  // Load the new date's note
+  const newBuffer = notesService.getNoteByDateFromLocalStorage(date)
+  store.dispatch(changeBuffer(newBuffer))
+  // Proceed to the action creation
   return {
     type: types.CHANGE_DATE,
     date,
